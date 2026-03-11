4afe94fc86bda7f5d0588bfd7ada8b5ea2a749a5
Bertrand2808 <bertrand2808@icloud.com>
2026-03-11 11:12:56 +0100

    td1 : work in progress to step 3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Cards } from './pages/cards/cards';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home }, // http://localhost:4200/
+  { path: 'cards', component: Cards }, // http://localhost:4200/cards
+  { path: '**', redirectTo: '', pathMatch: 'full' }   // tout le reste → redirige vers accueil (page 404 soft)
+];
