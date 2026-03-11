8043a8721d03b67c403186571509a0dbc1cc1078
Bertrand2808 <bertrand2808@icloud.com>
2026-03-11 11:22:48 +0100

    td1 : work in progress step 4 + add console log for search query

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -9,4 +9,12 @@ import { Component, signal} from '@angular/core';
 export class Home {
   // Signal pour stocker la valeur des inputs de recherche
   searchQuery = signal('');
+
+  onSearch(event: Event) {
+    const valueOfQuery = (event.target as HTMLInputElement).value;
+    console.log("Nouvelle valeur saisie : ", valueOfQuery);
+    this.searchQuery.set(valueOfQuery);
+    console.log("Signal mis à jour : ", this.searchQuery());
+  }
+
 }
